e255577dcc02e1ad98ff5cee8c31d0ad0c9cb58c
Wesley <wesleyreid0@gmail.com>
2020-12-22 22:57:06 -0500

    Create day_1-pt1.py

---
+++ day_1-pt1.py
@@ -0,0 +1,14 @@
+import math 
+
+file = open('day1.txt', 'r')
+
+fuel = []
+
+for line in file:
+    mass = int(line.strip())
+    first_step = mass / 3
+    second_step = math.floor(first_step)
+    required_fuel = second_step - 2
+    fuel.append(required_fuel)
+
+print(sum(fuel))
